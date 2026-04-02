6adaac013c2876e56a957d70ff6d287b0aad15c0
Alex Kaktus <aurabyte.dev@gmail.com>
2026-04-02 12:41:31 +0200

    refactor: remove analysis and main

--- analysis.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data/salaries.csv')
-df.head()
-print(len(df))
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from data-visualisation!")
-
-
-if __name__ == "__main__":
-    main()
